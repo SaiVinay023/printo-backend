e8645cd9734886136be8218d0acd358c9d446a7f
saivinay0carsu <saivinay@carsu.com>
2025-10-17 03:24:28 +0200

    fix: placed verify 2fa outside of sanctum:

--- routes/api.php
+++ routes/api.php
@@ -7,10 +7,10 @@ use App\Http\Controllers\AuthController;
 // Registration and login do not require authentication
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-
+Route::post('/verify-2fa', [AuthController::class, 'verify2FA']);
 Route::middleware('auth:sanctum')->group(function() {
     Route::post('/setup-2fa', [AuthController::class, 'setup2FA']);
-    Route::post('/verify-2fa', [AuthController::class, 'verify2FA']);
+    
     Route::get('/profile', [AuthController::class, 'profile']);
 });
 
